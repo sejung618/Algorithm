29a56850cea182dd6176c61332b13ec4ef1aad36
sejung618 <151819841+sejung618@users.noreply.github.com>
2024-04-09 21:09:35 +0900

    Update N개의 최소공배수.java

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,34 +1,28 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] arr) {
-        int answer = 0;
+	public int solution(int[] arr) {
+		int answer = 0;
 
-        Arrays.sort(arr);
-        int big = arr[arr.length-1];
-        int index = arr.length-1;
-        while(true)
-        {
-            boolean check = true;
+		Arrays.sort(arr);
+		int big = arr[arr.length - 1];
+		int index = arr.length - 1;
+		while (true) {
+			boolean check = true;
 
-            for(int i=0; i<index; i++)
-            {
-                if(big%arr[i]!=0)
-                {
-                    check = false;
-                }
-                else
-                {
+			for (int i = 0; i < index; i++) {
+				if (big % arr[i] != 0) {
+					check = false;
+				} else {
 
-                }
-            }
-            if(check==true)
-            {
-                answer = big;
-                break;
-            }
-            big = big+arr[arr.length-1];
-        }
-        return answer;
-    }
+				}
+			}
+			if (check == true) {
+				answer = big;
+				break;
+			}
+			big = big + arr[arr.length - 1];
+		}
+		return answer;
+	}
 }

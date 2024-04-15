4f21085f97718876cb70e2825994e36bff1b4e6b
sejung618 <151819841+sejung618@users.noreply.github.com>
2024-04-15 23:30:08 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,28 +1,11 @@
 class Solution {
 	public int solution(int n, int a, int b) {
 		int answer = 0;
-		String maxa = Integer.toBinaryString(n);
-		int max = maxa.length() - 1;
-		int numa = a>b ? b:a;
-		int numb = a>b ? a:b;
-		
-		for (int i = 0; i < max; i++) {
-
-			if (numa > Math.pow(2, max - 1 - i) && numb > Math.pow(2, max - 1 - i)) {
-				numa = (int) (numa - Math.pow(2, max -1- i));
-				numb = (int) (numb - Math.pow(2, max -1- i));
-			}
-
-			if (numa <= Math.pow(2, max - 1 - i) && numb <= Math.pow(2, max - 1 - i)) {
-				continue;
-			}
-
-			if (numa <= Math.pow(2, max - 1 - i) && numb > Math.pow(2, max - 1 - i)) {
-				return max - i;
-			}
-
+		while (a != b) {
+			a = a / 2 + a % 2;
+			b = b / 2 + b % 2;
+			answer++;
 		}
-
 		return answer;
 	}
 }

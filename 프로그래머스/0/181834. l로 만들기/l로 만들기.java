a4f698670486dce5744dc7dfd334a484aaf205e7
sejung618 <151819841+sejung618@users.noreply.github.com>
2024-05-09 07:55:44 +0900

    [level 0] Title: l로 만들기, Time: 620.38 ms, Memory: 379 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,13 +1,15 @@
 class Solution {
     public String solution(String myString) {
     	String answer = "";
-        char[] aa = myString.toCharArray();
-        for(char ch:aa) {
-        	if(ch <'l') {
-        		ch = 'l';
-        	}
-        	answer += ch;
-        }
+        for (int i = 0; i < myString.length(); i++) {
+			if(myString.charAt(i)<'l') {
+				answer += 'l';
+			}else {
+				answer += myString.charAt(i);
+			}
+		}
+        
+    	
         return answer;
     }
 }
